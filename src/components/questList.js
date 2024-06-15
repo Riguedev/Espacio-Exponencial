872ea5bc0f4641a9448bdc3d5d98b43f57d8a5b8
Jesús Rico <riguedev@gmail.com>
2024-06-14 19:52:48 -0600

    Cabio de la pregunta de prueba por las preguntas finales

--- src/components/questList.js
+++ src/components/questList.js
@@ -1,32 +1,30 @@
 export const questionList = [
     {
         questionInfo: {
-            title: "Quien posee la tecnica de mimetismo",
+            title: "La Grafica de la función exponencial f(x) = (a)x pasa por los puntos:",
             options: [
-                { res: "Yuta", distance: 4700 },
-                { res: "Gojo", distance: 4750 },
-                { res: "Maki", distance: 4800 },
-                { res: "Miwa", distance: 4850 },
-                { res: "Geto", distance: 4900 }
+                { res: "(1,0)(1,0)", distance: 4700 },
+                { res: "(3,0)(2,0)", distance: 4750 },
+                { res: "(0,1)(1,9)", distance: 4800 },
+                { res: "(0.0)(0,0)", distance: 4850 },
             ],
-            result: "Yuta"
+            result: "(0,1)(1,9)"
         },
 
-        positionX: 900,
+        positionX: 800,
         positionY: 4600
     },
 
     {
         questionInfo: {
-            title: "Quien posee la tecnica de mimetismo",
+            title: "Selecciona la respuesta correcta para la ecuación (5)x = 25",
             options: [
-                { res: "Yuta", distance: 3700 },
-                { res: "Gojo", distance: 3750 },
-                { res: "Maki", distance: 3800 },
-                { res: "Miwa", distance: 3850 },
-                { res: "Geto", distance: 3900 }
+                { res: "x = 5", distance: 3700 },
+                { res: "x = 0", distance: 3750 },
+                { res: "x = -2", distance: 3800 },
+                { res: "x = 2", distance: 3850 },
             ],
-            result: "Yuta"
+            result: "x = 2"
         },
 
         positionX: 1400,
@@ -35,15 +33,14 @@ export const questionList = [
 
     {
         questionInfo: {
-            title: "Quien posee la tecnica de mimetismo",
+            title: "Formula general de una función exponencial",
             options: [
-                { res: "Yuta", distance: 2700 },
-                { res: "Gojo", distance: 2750 },
-                { res: "Maki", distance: 2800 },
-                { res: "Miwa", distance: 2850 },
-                { res: "Geto", distance: 2900 }
+                { res: "f(x) = (a)x", distance: 2700 },
+                { res: "f(a) = (2)3", distance: 2750 },
+                { res: "f = raiz de 2", distance: 2800 },
+                { res: "f = (0,1)(0,3)", distance: 2850 },
             ],
-            result: "Yuta"
+            result: "f(x) = (a)x"
         },
 
         positionX: 1900,
@@ -52,15 +49,14 @@ export const questionList = [
 
     {
         questionInfo: {
-            title: "Quien posee la tecnica de mimetismo",
+            title: "La función exponencial es decreciente cuando",
             options: [
-                { res: "Yuta", distance: 1600 },
-                { res: "Gojo", distance: 1650 },
-                { res: "Maki", distance: 1700 },
-                { res: "Miwa", distance: 1750 },
-                { res: "Geto", distance: 1800 }
+                { res: "0 > a > 1", distance: 1600 },
+                { res: "0 > a < 1", distance: 1650 },
+                { res: "0 < a < 1", distance: 1700 },
+                { res: "0 < a > 1", distance: 1750 },
             ],
-            result: "Yuta"
+            result: "0 < a < 1"
         },
 
         positionX: 2400,
@@ -69,15 +65,14 @@ export const questionList = [
 
     {
         questionInfo: {
-            title: "Quien posee la tecnica de mimetismo",
+            title: "Encuentra la solución a la siguiente ecuación: (1/9)x = 81",
             options: [
-                { res: "Yuta", distance: 700 },
-                { res: "Gojo", distance: 750 },
-                { res: "Maki", distance: 800 },
-                { res: "Miwa", distance: 850 },
-                { res: "Geto", distance: 900 }
+                { res: "x = 9", distance: 700 },
+                { res: "x = -2", distance: 750 },
+                { res: "x = 2", distance: 800 },
+                { res: "x = -(2/3)", distance: 850 },
             ],
-            result: "Yuta"
+            result: "x = -2"
         },
 
         positionX: 3100,
